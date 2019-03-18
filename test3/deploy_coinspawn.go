19e886893d0ef9f3b32a907002fa88e192890503
NGUYEN THANH BINH <nguyenthanhbinh@NGUYENs-MacBook-Pro.local>
2019-03-18 16:21:29 +0700

    add utils folder

--- test3/deploy_coinspawn.go
+++ test3/deploy_coinspawn.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"test_eth/contracts/metacoin"
+	"test_eth/test3/lib"
 		// "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
