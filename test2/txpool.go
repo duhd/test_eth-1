1dfbe4011529162fd395a40801f1162aa935baac
NGUYEN THANH BINH <nguyenthanhbinh@NGUYENs-MacBook-Pro.local>
2019-03-27 15:17:49 +0700

    add txpool

--- test2/txpool.go
+++ test2/txpool.go
@@ -2,33 +2,62 @@ package main
 
 import (
 	"os"
-	"strings"
+	// "strings"
 	"context"
 	"log"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/p2p"
+	// "github.com/ethereum/go-ethereum/p2p"
 	"fmt"
-	"test_eth/test2/utils"
+	// "test_eth/test2/utils"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	// "github.com/ethereum/go-ethereum/core/rawdb"
+	// "github.com/ethereum/go-ethereum/core/types"
+	// "github.com/ethereum/go-ethereum/core/vm"
+	// "github.com/ethereum/go-ethereum/crypto"
+	// "github.com/ethereum/go-ethereum/log"
+	// "github.com/ethereum/go-ethereum/p2p"
+	// "github.com/ethereum/go-ethereum/params"
+	// "github.com/ethereum/go-ethereum/rlp"
+	// "github.com/ethereum/go-ethereum/rpc"
+
 )
 
-var cfg *utils.Config
+
+type RPCTransaction struct {
+	BlockHash        common.Hash     `json:"blockHash"`
+	BlockNumber      *hexutil.Big    `json:"blockNumber"`
+	From             common.Address  `json:"from"`
+	Gas              hexutil.Uint64  `json:"gas"`
+	GasPrice         *hexutil.Big    `json:"gasPrice"`
+	Hash             common.Hash     `json:"hash"`
+	Input            hexutil.Bytes   `json:"input"`
+	Nonce            hexutil.Uint64  `json:"nonce"`
+	To               *common.Address `json:"to"`
+	TransactionIndex hexutil.Uint    `json:"transactionIndex"`
+	Value            *hexutil.Big    `json:"value"`
+	V                *hexutil.Big    `json:"v"`
+	R                *hexutil.Big    `json:"r"`
+	S                *hexutil.Big    `json:"s"`
+}
+
 
 func main(){
-	if len(os.Args) == 1 {
-		 fmt.Println("Please use syntax: go run txpool.go  server ")
-		 server = os.Args[1]
-	}
-	ctx := context.Background()
-	method := "txpool_content"
-	client, err := rpc.DialContext(ctx, server)
-	if err != nil {
-		log.Fatalf("Unable to connect to network:%v\n", err)
-	}
+		if len(os.Args) != 2 {
+			 fmt.Println("Please use syntax: go run txpool.go  server ")
+			 return
+		}
+	  server := os.Args[1]
+		ctx := context.Background()
+		method := "txpool_content"
+		client, err := rpc.DialContext(ctx, server)
+		if err != nil {
+			log.Fatalf("Unable to connect to network:%v\n", err)
+		}
+		var result map[string]map[string]map[string][]*RPCTransaction
+		client.CallContext(ctx, &result, method)
 
-	client.CallContext(ctx, &result, method)
-	enode := strings.Replace(result.Enode,"127.0.0.1",node.LocalAddr,-1)
-	enode = strings.Replace(enode,"?discport=0","",-1)
-	fmt.Printf("Enode: ", enode)
-	enodes = append(enodes,enode)
-	clients = append(clients,client)
+		for key, value := range result {
+	    fmt.Println("Key:", key, "Value:", value)
+		}
 }
